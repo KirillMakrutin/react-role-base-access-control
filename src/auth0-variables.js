c2a960bc8dfd149c38ebc4754ad818a40fe14257
kmakrutin <k.makrutin@gmail.com>
2020-04-03 18:43:31 +0300

    RBAC initial

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "blog-samples.auth0.com",
+  roleUrl: "https://rbac-tutorial-app/role",
+  clientId: "rjVZ1xa8163rYtvK8tQn9ou0p6YdmMGZ",
+  callbackUrl: "http://localhost:3000/callback",
+};
